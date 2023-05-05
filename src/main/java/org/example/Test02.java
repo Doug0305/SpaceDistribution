df0c4d116261a26ffcc3852ce9cbf48d93827a37
Li Dongyun <905422641@qq.com>
2023-05-05 12:18:25 +0800

    修改目录结构

--- src/main/java/org/example/Test02.java
+++ src/main/java/org/example/Test02.java
@@ -24,13 +24,13 @@ import java.util.Map;
  * @author: Donggeng
  * @create: 2023-02-22 21:08
  */
-public class Test01 extends PApplet {
+public class Test02 extends PApplet {
     public static void main(String[] args) {
         PApplet.main("org.example.Test01");
     }
 
     CameraController cam;
-    LineSegment[] baseline;
+    LineSegment[] baseline = new LineSegment[4];
     BaseLineGroup baseLineGroup;
 
     public void settings() {
@@ -51,32 +51,9 @@ public class Test01 extends PApplet {
         RoomLoader loader = new RoomLoader(path, 0);
         List<Room> list1 = loader.getList();
         //建筑线段自定义初始化
-        Coordinate c0 = new Coordinate(0, 0, 0);
-        Coordinate c1 = new Coordinate(60, 0, 0);
-        Coordinate c2 = new Coordinate(60, 80, 0);
-        Coordinate c3 = new Coordinate(-10, 60, 0);
-        Coordinate c4 = new Coordinate(-30, 0, 0);
-        Coordinate c5 = new Coordinate(4, -30, 0);
-        Coordinate c6 = new Coordinate(60, -40, 0);
-        Coordinate c7 = new Coordinate(90, 0, 0);
-        Coordinate c8 = new Coordinate(90, 90, 0);
-        Coordinate c9 = new Coordinate(60, 120, 0);
-        Coordinate c10 = new Coordinate(-20, 100, 0);
-        Coordinate c11 = new Coordinate(-40, 60, 0);
-        baseline = new LineSegment[12];
-        baseline[0] = new LineSegment(c0,c1);
-        baseline[1] = new LineSegment(c1,c2);
-        baseline[2] = new LineSegment(c2,c3);
-        baseline[3] = new LineSegment(c3,c0);
-        baseline[4] = new LineSegment(c0,c4);
-        baseline[5] = new LineSegment(c0,c5);
-        baseline[6] = new LineSegment(c1,c6);
-        baseline[7] = new LineSegment(c1,c7);
-        baseline[8] = new LineSegment(c2,c8);
-        baseline[9] = new LineSegment(c2,c9);
-        baseline[10] = new LineSegment(c3,c10);
-        baseline[11] = new LineSegment(c3,c11);
-
+        for (int i = 0; i < baseline.length; i++) {
+            baseline[i] = new LineSegment(0, i * 80, 80, i * 90);
+        }
         baseLineGroup = new BaseLineGroup(baseline, loader.minArea, loader.maxArea, 0.3, 4, 2, 8, 8);
 
         //设置噪音源及推荐远离距离
@@ -122,6 +99,7 @@ public class Test01 extends PApplet {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
         time = formatter.format(new Date(System.currentTimeMillis()));
     }
+
     @Override
     public void keyReleased() {
         if (key == 's' || key == 'S') showLight = !showLight;
